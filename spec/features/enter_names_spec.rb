4184abb5fc0431fd3158f614ad122e7aaee43cdf
Elena Morton <elmo@Elenas-MacBook-Pro.local>
2017-05-24 15:39:57 +0100

    Adding web helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -9,10 +9,7 @@
 
 feature "Enter names" do
   scenario "Create name users and display them" do
-    visit('/')
-    fill_in('player_1_name', with: 'Trump')
-    fill_in('player_2_name', with: 'Putin')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('Trump')
     expect(page).to have_content('Putin')
   end
--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -1,10 +1,6 @@
 feature 'View hit points' do
   scenario 'View player 2 hit points' do
-    visit('/')
-    fill_in('player_1_name', with: 'Trump')
-    fill_in('player_2_name', with: 'Putin')
-    click_button('Submit')
-
+    sign_in_and_play
     expect(page).to have_content('Trump vs. Putin Putin: 60HP')
   end
 end
